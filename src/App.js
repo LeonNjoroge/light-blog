0b0724fe1babbcfdfbe148f6dcac7811264071a7
LeonNjoroge <leonmuraguri@gmail.com>
2024-09-29 01:48:53 +0300

    Style Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+    <div className = "App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
-
 export default App;
+
+/* First Editted APP
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  //const person = {name: "Dude", age:30};
+  const link = "https://google.com";
+  return (
+    <div className="App">
+     <div className = "content">
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
+      <p>{10}</p>
+      <p>{'hello ninjas'}</p>
+      <p>[1,2,3,4]</p>
+      <p>{Math.random()*10}</p>
+
+      <a href={link}>Google Site</a>
+     </div>
+    </div>
+  );
+}
+*/
