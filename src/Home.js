c3d0db244fda33401bed76a162fc6041127e2a7c
LeonNjoroge <leonmuraguri@gmail.com>
2024-09-29 03:55:36 +0300

    Change useState

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,27 @@
+import {useState} from 'react';
+
+const Home = () =>{
+  //  let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(24);
+    const handleClick = () =>{
+      /* name='luigi';
+       console.log(name);
+       */
+      setName('luigi');
+      setAge(32);
+    }
+
+    return(
+        <div className = "home">
+            <h2>Home Page</h2>
+            <p>{name} is {age} years old.</p>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    );
+}
+
+/* Cheking different Component uses
 const Home = () =>{
     const handleClick = (e) =>{
         console.log("Hello, Ninjas", e);
@@ -11,8 +35,11 @@ const Home = () =>{
         <div className = "home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click me</button>
+
             <button onClick={(e)=>handleClickAgain('mario', e)}>Click me Again</button>
         </div>
     );
 }
+    */
+
 export default Home;
